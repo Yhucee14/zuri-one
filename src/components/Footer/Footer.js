656cb1487e08b4bba654784db81dc1fe5d85100d
uchenna <uchennachukwu2002@yahoo.com>
2022-10-31 21:01:36 -0700

    Updated links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,16 +6,16 @@ const Footer = () => {
   return (
     <div className='footer'>
          <h4 id='left'>
-        <a href='#'>Zuri<b className='col'>.</b>Internship</a>
+        <a href='hngi9.zuriboard.com'>Zuri<b className='col'>.</b>Internship</a>
         </h4>
 
         <h6 id='center'>
-        <a href='#'><b className='light'>HNG Internship 9 Frontend Task</b></a>
+        <a href='https://hng9.notion.site/Front-End-Stage-1-Task-cdaf503a796b4a5a85c5883a0a6163db'><b className='light'>HNG Internship 9 Frontend Task</b></a>
         </h6>
 
         <h5 id='right'> 
         <img src={map} alt='any' id='logo-img' />
-        <a href='#'>INGRESSIVE <br />FOR GOOD</a>
+        <a href='hngi9.zuriboard.com'>INGRESSIVE <br />FOR GOOD</a>
         </h5>
 
     </div>
