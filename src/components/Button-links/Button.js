bc8fbdc65b5988ce1f37994ac0cc793cb4bf4262
uchenna <uchennachukwu2002@yahoo.com>
2022-11-03 04:44:05 -0700

    feat: Added a new anchor tag named

--- src/components/Button-links/Button.js
+++ src/components/Button-links/Button.js
@@ -11,6 +11,7 @@ const Button = () => {
         <a href='https://books.zuri.team/python-for-beginners?ref_id=<Ucee>' id='book_python' title="The HNG basic coding 1.0 gives you the best fundamental knowledge in demand.With a fair price of $4.99, starting your tech journey with this book certainly gives you an advantage.">Python Books</a>
         <a href='https://background.zuri.team/' id='pitch' title="This gives you access to the best potential employees by conducting a thorough and intricate background check on their past endeavours.">Background Check for Coders</a>
         <a href='https://books.zuri.team/design-rules' id='book_design' title="The free design book offers you the best design knowledge in demand for absolutely no cost.">Design Books </a>
+        <a href='https://books.zuri.team/design-rules' id='contact' title="Click here to Contact Me.">Contact Me </a>
 
         <div>
           
