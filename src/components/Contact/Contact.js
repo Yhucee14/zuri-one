7fa2a1cad1b6b225a8a6933b3b93f53ae3fcf831
uchenna <uchennachukwu2002@yahoo.com>
2022-11-04 07:04:35 -0700

    Modified the form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,30 +6,32 @@ const Contact = () => {
   return (
     <div className='form-wrap'> 
     <form className='form-cover'>
+    <h1><b>Contact Me.</b></h1>
+    Hi there, contact me to ask me about anything you have in mind. <br></br><br></br><br></br>
       <div className='name'>
         <div className='first'>
         <label for="name">First name </label> <br></br>
-          <input type="text" name="name" id="first-input1" placeholder="First name" /><br></br>
+          <input type="text" name="name" id="first_name" placeholder="First name" /><br></br>
         </div>
 
         <div className='last'>
           <label for="name">Last name </label> <br></br>
-            <input type="text" name="name" id="first-input2" placeholder="last name" />
+            <input type="text" name="name" id="last_name" placeholder="last name" />
         </div>
       </div> <br></br>
 
 
     <label for="Email">Email</label> <br></br>
-    <input type="Email" name="" id="email-input" placeholder="Email@email.com" /> <br></br>
+    <input type="Email" name="" id="email" placeholder="Email@email.com" /> <br></br>
 
     <p>Message</p>
-    <textarea name="" id="msg" cols="20" rows="8"></textarea>
+    <textarea name="" id="message" cols="20" rows="8" placeholder="Send me a message and I'll reply you as soon as possible."></textarea>
     <br/><br></br>
 
     <input type="checkbox" value="check" name="check" id="check"/> You Agree to providing your data to (name) who may contact you. <br></br> <br></br>
 
     <div className='send-cont'>
-        <a href='#' id='send' title="Click here to to Send Message."> Send Message</a>
+        <button href='#' id='btn_submit' title="Click here to to Send Message."> Send Message</button>
     </div>
     
       
